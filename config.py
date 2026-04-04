b029715de65d5a5608161cd753f669a61292bb9f
anujedits42-byte <anujedits42@gmail.com>
2026-04-04 19:35:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ""))
-    API_HASH = os.getenv("API_HASH", "")
+    API_ID = int(os.getenv("API_ID", "34446649"))
+    API_HASH = os.getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
     
     # Database
-    MONGO_URL = os.getenv("MONGO_URL", "")
+    MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
     
     # Admin
-    ADMIN_STR = os.getenv("ADMINS", "")
+    ADMIN_STR = os.getenv("ADMINS", "7892805795")
     ADMINS = [int(x) for x in ADMIN_STR.split()] if ADMIN_STR else []
     
     # Channel
-    FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "") # username or ID
+    FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "log_channel_a") # username or ID
     
     # UI
     START_PIC = os.getenv("START_PIC", "https://ibb.co/XZZhqDs0") # Default image
     
     # Log Channel
-    LOG_CHANNEL = os.getenv("LOG_CHANNEL", "") # Channel ID (e.g. -100xxxx)
+    LOG_CHANNEL = os.getenv("LOG_CHANNEL", "-1003515041061") # Channel ID (e.g. -100xxxx)
 
