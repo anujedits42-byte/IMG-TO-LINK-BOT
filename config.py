70abac880b0cb36d3675f435170fc3b410e5e4d5
anujedits42-byte <anujedits42@gmail.com>
2026-04-14 21:35:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     API_ID = int(os.getenv("API_ID", "34446649"))
     API_HASH = os.getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "8337679965:AAFyI31rSKDe7dzNAlvD-lDQsdwfO_TlqPs")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8790821040:AAGcPj2-zuPpIq9CwktrrlpkwpGtVuaVkAg")
     
     # Database
     MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
